564f946fb4a1903e7313cbcdfd92a66236009311
Mis4nthr0pic <121455563+Mis4nthr0pic@users.noreply.github.com>
2023-07-07 21:34:57 -0300

    Update GatekeeperTwo.t.sol

--- src/test/GatekeeperTwo.t.sol
+++ src/test/GatekeeperTwo.t.sol
@@ -16,33 +16,6 @@ contract GatekeeperTwoTest is DSTest {
     }
 
     function testGatekeeperTwoHack() public {
-        /////////////////
-        // LEVEL SETUP //
-        /////////////////
-
-        GatekeeperTwoFactory gatekeeperTwoFactory = new GatekeeperTwoFactory();
-        ethernaut.registerLevel(gatekeeperTwoFactory);
-        vm.startPrank(tx.origin);
-        address levelAddress = ethernaut.createLevelInstance(gatekeeperTwoFactory);
-        GatekeeperTwo ethernautGatekeeperTwo = GatekeeperTwo(payable(levelAddress));
-        vm.stopPrank();
-
-        //////////////////
-        // LEVEL ATTACK //
-        //////////////////
-
-    
-        // Create attacking contract - attack is inside the constructor so no need to call any subsequent functions
-        GatekeeperTwoHack gatekeeperTwoHack = new GatekeeperTwoHack(levelAddress);
         
-
-        //////////////////////
-        // LEVEL SUBMISSION //
-        //////////////////////
-
-        vm.startPrank(tx.origin);
-        bool levelSuccessfullyPassed = ethernaut.submitLevelInstance(payable(levelAddress));
-        vm.stopPrank();
-        assert(levelSuccessfullyPassed);
     }
 }
