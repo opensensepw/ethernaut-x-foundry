1e2f4b7e455c9411b9066b1798bca616bc57eeec
Mis4nthr0pic <121455563+Mis4nthr0pic@users.noreply.github.com>
2023-07-07 21:30:31 -0300

    Update DexTwo.t.sol

--- src/test/DexTwo.t.sol
+++ src/test/DexTwo.t.sol
@@ -14,34 +14,6 @@ contract DexTwoTest is DSTest {
     }
 
     function testDexTwoHack() public {
-        /////////////////
-        // LEVEL SETUP //
-        /////////////////
-
-        DexTwoFactory dexTwoFactory = new DexTwoFactory();
-        ethernaut.registerLevel(dexTwoFactory);
-        address levelAddress = ethernaut.createLevelInstance{value: 1 ether}(dexTwoFactory);
-        DexTwo ethernautDexTwo = DexTwo(payable(levelAddress));
-
-        //////////////////
-        // LEVEL ATTACK //
-        //////////////////
-
-        // Create DexTwoHack Contract
-        DexTwoHack dexTwoHack = new DexTwoHack(ethernautDexTwo);
-        
-        // give the attack contract the balance
-        IERC20(ethernautDexTwo.token1()).transfer(address(dexTwoHack), IERC20(ethernautDexTwo.token1()).balanceOf(address(this)));
-        IERC20(ethernautDexTwo.token2()).transfer(address(dexTwoHack), IERC20(ethernautDexTwo.token2()).balanceOf(address(this)));
-
-        // Call the attack function
-        dexTwoHack.attack();
-
-        //////////////////////
-        // LEVEL SUBMISSION //
-        //////////////////////
-
-        bool levelSuccessfullyPassed = ethernaut.submitLevelInstance(payable(levelAddress));
-        assert(levelSuccessfullyPassed);
+       
     }
 }
