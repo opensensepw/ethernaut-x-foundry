b15c971fbe740bd2db2cdce66a1229d2284581b9
Mis4nthr0pic <121455563+Mis4nthr0pic@users.noreply.github.com>
2023-07-07 21:31:33 -0300

    Update Force.t.sol

--- src/test/Force.t.sol
+++ src/test/Force.t.sol
@@ -20,31 +20,6 @@ contract ForceTest is DSTest {
 
     function testForceHack() public {
 
-        /////////////////
-        // LEVEL SETUP //
-        /////////////////
-
-        ForceFactory forceFactory = new ForceFactory();
-        ethernaut.registerLevel(forceFactory);
-        vm.startPrank(eoaAddress);
-        address levelAddress = ethernaut.createLevelInstance(forceFactory);
-        Force ethernautForce = Force(payable(levelAddress));
-
-
-        //////////////////
-        // LEVEL ATTACK //
-        //////////////////
-
-        // Create the attacking contract which will self destruct and send ether to the Force contract
-        ForceHack forceHack = (new ForceHack){value: 0.1 ether}(payable(levelAddress));
-
-
-        //////////////////////
-        // LEVEL SUBMISSION //
-        //////////////////////
-
-        bool levelSuccessfullyPassed = ethernaut.submitLevelInstance(payable(levelAddress));
-        vm.stopPrank();
-        assert(levelSuccessfullyPassed);
+      
     }
 }
